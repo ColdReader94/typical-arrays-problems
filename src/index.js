01cc69402db750aca1d8e1bc36ce810836f31005
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-09-30 10:13:28 +0300

    feat: add solution for all 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0) return 0;
+  else { 
+  let minValue = array[0];
+  for(let i = 0; i < array.length; i++) {
+    if([array[i]] < minValue) {
+      minValue = array[i];
+    }
+  }
+  return minValue;
+}  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0) return 0;
+  else { 
+  let maxValue = array[0];
+  for(let i = 0; i < array.length; i++) {
+    if([array[i]] > maxValue) {
+      maxValue = array[i];
+    }
+  }
+  return maxValue;
+}  
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+  if(typeof array == 'undefined' || array.length == 0) return 0;
+  else {  
+    let average = 0;  
+    for(let i = 0; i < array.length; i++) {
+      average += array[i];
+    }
+    return average / array.length;
+}
 }
